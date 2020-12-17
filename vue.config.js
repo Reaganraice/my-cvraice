08bc072413ed97f29ee706f3a4b3086319bff022
Reagan Matunga <reaganmatunga@gmail.com>
2020-12-18 00:08:07 +0200

    vuetify

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,15 @@ module.exports = {
       localeDir: 'locales',
       enableInSFC: false
     }
+  },
+
+  chainWebpack: config => {
+    config.plugin('VuetifyLoaderPlugin').tap(args => [{
+      match (originalTag, { kebabTag, camelTag, path, component }) {
+        if (kebabTag.startsWith('core-')) {
+          return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+        }
+      }
+    }])
   }
 }
